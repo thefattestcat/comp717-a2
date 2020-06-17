6b5a0d3e9c10d2788a1f67e53945850b47fb80a4
thefattestcat <48980192+thefattestcat@users.noreply.github.com>
2020-06-17 13:23:32 +1200

    AI working?

--- src/games/Tictactoe/Tictactoe.jsx
+++ src/games/Tictactoe/Tictactoe.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react';
 import {Container, Row, Col, Button} from 'react-bootstrap';
 import GridSquare from './gridSquare'
 import './Tictactoe.css'
@@ -7,62 +9,102 @@ class Tictactoe extends Component {
 
   constructor(props) {
     super(props);
-    
+    this.gs = []
     this.state = {
-      turnCount: 0, //Current turn
+      turnCount: 1, //Current turn
       whosTurn: 'Your turn', 
       moveArray: [],  //Array of Moves
       positionState: [], //Who owns each square (0-8), 0 = empty, 1 = player, 2 = AI
       mode: '', //Not implemented (AI or 2-PLayer)
-      
+      startingPlayer: 'Player'
     };
-                
+
   }
 
   componentDidMount() {
-    
+    console.log(React.version)
   }
 
   newGame = () => {
     let p = []
     for(let i = 0; i < 9; i++)
       p[i] = 0;
+    
     this.setState( {
-      turnCount: 0,
+      turnCount: 1,
       whosTurn: '',
       moveArray: [],
       positionState: p,
+      winner: '',
     }, () => {
       this.setState({
         grid: this.createGrid(),
+      }, () => {
+        console.log('Grid mounted')
+        console.log(this.state.grid)
+        
+        console.log('Game Started!')
+        if(this.state.startingPlayer == 'AI') {
+          let d = Math.floor(Math.random() * 10)
+          this.gs[d].current.handleAI();
+        }
       })
-      console.log('Grid mounted')
-      console.log(this.state.grid)
     })
   }
+  
+  checkPosition = (id) => {
+    return this.state.moveArray.indexOf(id) == -1;
+  }
+  
+  checkWin = () => {
+    //Get positionState and do calculations for winner
+    let p = this.state.positionState;
+    let w;
+    //make this func
+    if(
+      (p[0] == 1 && p[1] == 1 && p[2] == 1) ||
+      (p[0] == 1 && p[4] == 1 && p[8] == 1) ||
+      (p[0] == 1 && p[3] == 1 && p[6] == 1) ||
+      (p[0] == 1 && p[1] == 1 && p[2] == 1) 
+      ){ w = 'Player wins';}
+    else w = 'Noone';
+    this.setState({
+      winner: w,
+    })
+    return w;
+  }
 
-  squaresCB = (state, cb) => {
+  setSquareState = (squareId, playerId) => {
     return new Promise ((resolve, reject) => {
-      if(this.state.moveArray.indexOf(state.id) == -1) {
-        let j = this.state.moveArray.concat(state.id)
+      if(this.checkPosition(squareId)) {
+        let j = this.state.moveArray.concat(squareId)
         let k = this.state.positionState;
-        k[state.id] = 1;
+        k[squareId] = playerId;
         this.setState({ 
           moveArray: j,
           positionState: k
         }, () => {
           console.log(this.state.moveArray);
           console.log(this.state.positionState);
+          if(this.state.moveArray.length >= 5) {
+            this.checkWin()
+          }
           this.incrementTurn();
-          resolve(this.state.positionState[state.id])
+          resolve(this.state.positionState[squareId])
         }) 
       } else {
         console.log('Position already filled')
-        reject(this.state.positionState[state.id]) 
+        reject(this.state.positionState[squareId]) 
       }
     })
-    //If square id is not in moveArray
-    
+  }
+
+  squaresCB = (state) => {
+    return this.setSquareState(state.id, 1); //(positionId, playerId= 1, for player)
+  }
+
+  aiMove = (id) => {
+    return this.setSquareState(id, 2);
   }
 
   incrementTurn = () => {
@@ -73,21 +115,37 @@ class Tictactoe extends Component {
     }
   }
 
+  whoStart = () => {
+    let p = this.state.startingPlayer;
+    if(p == 'Player') p = 'AI'
+    else p = 'Player';
+    this.setState({
+      startingPlayer: p
+    })
+  }
+
   //**Div stuff */
   createGrid = () => {
     let sqs = [];
-    for(var i = 0; i < 9; i++) 
+    for(var i = 0; i < 9; i++) {
+      this.gs[i] = React.createRef();
       sqs.push(
         <GridSquare 
-          parentCB={this.squaresCB} 
+          parentCB={this.squaresCB}
+          aiCB={this.aiMove} 
           id={i} 
           state={this.state.positionState[i]} 
+          player={0}
+          ref={this.gs[i]}
         />
       )
+    }
+    console.log(this.gs)
     return (sqs)
   }
    
   render() {
+    
     return (
       <>
       <Container>
@@ -97,8 +155,12 @@ class Tictactoe extends Component {
             <p>
               <b>Rules:</b><br></br> 
               In order to win the game, a player must place three of their marks in a horizontal, vertical, or diagonal row.
-              <br></br><b>Mode: {this.state.mode}</b>
+              <br></br>
+              The game checks for a winner from the 5th move onwards.
+              <br></br>
+              <b>Mode: {this.state.mode}</b>
             </p>
+            <Button onClick={this.whoStart}>Start: {this.state.startingPlayer}</Button>
             <Button onClick={this.newGame}>New Game</Button>
           </Col>
           <Col md="8" xs="12">
@@ -109,6 +171,7 @@ class Tictactoe extends Component {
             </div>
             <div style={{paddingTop: '20px'}}>
               <h3>Move #{this.state.turnCount}:  <span style={{color: "green"}}>{this.state.whosTurn}</span></h3>
+              <h3>Winner:   <span style={{color: "green"}}>{this.state.winner}</span></h3>
             </div>
             
           </Col>
--- src/games/Tictactoe/gridSquare.jsx
+++ src/games/Tictactoe/gridSquare.jsx
@@ -1,54 +1,94 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import './Tictactoe.css'
 
 class gridSquare extends Component {
 
-    constructor(props) {
-      super(props)
-      this.state = {
-        id: this.props.id,
-        color: this.props.color,
-        player: '', //Why am I tracking this here?
-      }
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: this.props.id,
+      player: 0, //Why am I tracking this here?
     }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleAI = this.handleAI.bind(this);
 
-    componentWillReceiveProps (props) {
-      this.changeCol(props)
-    }
-
-    handleClick = () => {
-      console.log('clicked ', this.state.id)
-      this.props.parentCB(this.state)
-        .then( (s) => {
-          this.changeCol(s)
-        })
-        .catch( (err) => {
-          console.log(err)
-        })
-    }
-
-    changeCol = (s) => {
-      console.log(s)
-      let c;
-      if(s == 0) c = 'white';
-      if(s == 1) c = 'red';
-      console.log('c is ', c)
-      this.setState({
-        color: c
-      })
-    }
-  
-    render() {
-      return (
-        <>
-          <div className="grid-square" 
-            onClick={this.handleClick} 
-            style={{backgroundColor: this.state.color}}>
-          </div>
-        </>
-      );
-    }
-    
   }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log(props, state)
+  }
+
+  componentDidMount() {
+    document.addEventListener('doAI', this.handleAI);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('doAI', this.handleAI);
+  }
+
+  componentDidUpdate() {
+    console.log('DidUpdate: ',this.state)
+  }
+
+  handleClick() {
+    console.log('clicked ', this.state.id)
+    this.props.parentCB(this.state)
+      .then( (s) => {
+        console.log(s)
+        this.setState({
+          player: s
+        }, () => {
+          this.changeCol();
+        })
+      })
+      .catch( (err) => {
+        console.log(err)
+      })
+  }
+
+  handleAI() {
+    console.log('HandleAI')
+    this.props.aiCB(this.state.id) 
+      .then( (s) => {
+        this.setState({
+          player: s
+        }, () => {
+          this.changeCol();
+        })
+      })
+      .catch( (err) => {
+        console.log(err)
+      })
+  }
+
+  changeCol = () => {
+    let s = this.state.player
+    console.log(s)
+    let c;
+    if(s == 0) c = 'white';
+    if(s == 1) c = 'red';
+    if(s == 2) c = 'blue';
+    console.log('c is ', c)
+    this.setState({
+      colour: c,
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <div className="grid-square" 
+          onClick={this.handleClick} 
+          style={{backgroundColor: this.state.colour}}
+          id={this.state.id}
+          ref={this.state.id}
+        >
+        </div>
+      </>
+    );
+  }
+    
+}
   
-  export default gridSquare;
+export default gridSquare;
