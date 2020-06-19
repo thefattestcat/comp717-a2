bd69b335bffd0724fb88e1506877428c489d3dea
thefattestcat <48980192+thefattestcat@users.noreply.github.com>
2020-06-19 13:00:05 +1200

    boardState Bug

--- src/games/Tictactoe/Tictactoe.jsx
+++ src/games/Tictactoe/Tictactoe.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import {Container, Row, Col, Button, InputGroup} from 'react-bootstrap';
+import {Container, Row, Col, Button} from 'react-bootstrap';
+import RangeSlider from 'react-bootstrap-range-slider';
+
 import GridSquare from './gridSquare'
 import './Tictactoe.css'
 
@@ -50,7 +52,8 @@ class Tictactoe extends Component {
           //this.aiMove();
         }
         let o = this.state.positionState
-        this.makeMove(o, 1,2)
+        this.makeMove(o, 1,2, this.state.depth)
+        console.log('Finished!')
       })
     })
   }
@@ -87,9 +90,8 @@ class Tictactoe extends Component {
           //console.log(this.state.moveArray);
           //console.log(this.state.positionState);
           if(this.state.moveArray.length >= 5) {
-            this.minmax();
-            if(this.checkTerminal())
-            this.checkWin()
+            if(this.checkTerminal(this.state.positionState))
+              this.checkWin(this.state.positionState)
           }
           resolve(this.state.positionState[squareId])
         }) 
@@ -129,11 +131,8 @@ class Tictactoe extends Component {
     
     console.log('availPos', availPos)
     if(availPos.length > 0){
-      //let d = availPos[Math.floor(Math.random() * availPos.length)]
-      //console.log('Moving to ', d)
-      //this.gs[d].current.handleAI();
       const o = this.state.positionState;
-      this.makeMove(o,1,2)
+      this.makeMove(o,1,2, this.state.depth)
     } else {
       console.log('no more moves')
     }
@@ -150,11 +149,9 @@ class Tictactoe extends Component {
    */
   checkEmptyPositions = (boardState) => {
     let emptyPositions = []; //Stores indexes of empty board positions.
-    
     for(let i = 0; i < boardState.length; i++) {
       if(boardState[i] === 0) emptyPositions.push(i);
     }
-    //console.log('checkEmpty:', boardState, emptyPositions)
     return emptyPositions;
   }
 
@@ -187,12 +184,16 @@ class Tictactoe extends Component {
     for (let i = 0; i < winIndexes.length; i++) {
       const [a, b, c] = winIndexes[i];
       if (boardState[a] !== 0 && boardState[a] === boardState[b] && boardState[a] === boardState[c]) {
-        return a; //Return playerId of square
+        return boardState[a]; 
       } 
     }
     return -1; //No winner
   }
   
+  /**
+   * 
+   * @param {postionStateArray} boardState - board state 
+   */
   checkTerminal = (boardState) => {
     if(this.checkEmptyPositions(boardState).length === 0) return true;
     if(this.checkWin(boardState) !== -1) return true;
@@ -210,39 +211,65 @@ class Tictactoe extends Component {
   makeMove = (boardState, p1, p2, depth) => {
     let x = this.state.minmaxLevel;
     x++;
-    const c = [...boardState];
     this.setState({minmaxLevel: x}, () => {
-      console.log(`minmax: ${this.state.minmaxLevel}  Current Player: ${p1}`)
       
-      if(!this.checkTerminal(boardState) && this.state.minmaxLevel < depth) {
+      if(this.state.minmaxLevel <= depth) {  
+        
+        if(this.checkTerminal(boardState)) {
+          const w = this.checkWin(boardState)
+          if(w === -1) console.log('Draw')
+          if(w === 0 ) console.log('bug: ', boardState, w)
+          else console.log('Game finished, winner= ', w)
+          return boardState;
+        } 
+
         let empty = this.checkEmptyPositions(boardState);
         if(empty.length > 0) {
           let k;
           for(let i = 0; i < empty.length; i++) {
-            k = [...c];
+            k = [...boardState];
             k[empty[i]] = p1;
-            if(this.checkTerminal(k)) {
-              const w = this.checkWin(k)
-              if(w === -1) console.log('Draw')
-              if(w === 1) console.log('Game finished, winner = ', w)
-              console.log(k)
-              return k;
-            } 
-            //console.log('makeMove: ', empty, k)
             this.makeMove(k, p2, p1, this.state.depth);
           } 
-        } 
-        else {
-          console.log('Game over, Winner = ', this.checkWin(c),c)
+        
+        
         }
       } 
-      
-      else {
-        console.log("Finished makeMove", boardState);
-        return boardState;
-      }
+
     })
   } 
+
+  /**
+   * 
+   * @param {positionStateArray} boardState - board state.
+   * @returns
+   * @description State evaluation function. 
+   * 
+   * Returns evaluation score from boardState. Scores it from -10 to 10 
+   * based upon the evaluation function given in the assignment documentation.
+   * 
+   * Eval(s) = 3*X2(s) + X1(s) - (3*O2(s) + O1(s))
+   */
+  stateEval = (boardState) => {
+    function X2 (boardState) {
+      const openLines = [
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8],
+      ];
+      for (let i = 0; i < openLines.length; i++) {
+        const [a, b, c] = openLines[i];
+        if (boardState[a] !== 2 && boardState[a] === boardState[b] && boardState[a] === boardState[c]){
+          return a;
+        } 
+      }
+    }
+  }
 
   minmax = () => {
     /**
@@ -266,7 +293,6 @@ class Tictactoe extends Component {
   createGrid = () => {
     let sqs = [];
     this.gs = [];
-    console.log('Grid: ', this.state)
     for(var i = 0; i < 9; i++) {
       this.gs[i] = React.createRef();
       sqs.push(
@@ -281,7 +307,6 @@ class Tictactoe extends Component {
         />
       )
     }
-    console.log(this.gs)
     return (sqs)
   }
    
@@ -302,7 +327,14 @@ class Tictactoe extends Component {
               <b>Mode: {this.state.mode}</b>
             </p>
             <div>
-              <p>Search Depth:</p>
+              <p>Search Depth: {this.state.depth}</p>
+              <RangeSlider
+                value={this.state.depth}
+                onChange={changeEvent => this.setState({depth: changeEvent.target.value})}
+                min={0}
+                max={7}
+                tooltipStyle={{display: 'none'}}
+              />
             </div>
             <Button onClick={this.whoStart}>Start: {this.state.startingPlayer}</Button>
             <Button onClick={this.newGame}>New Game</Button>
@@ -326,6 +358,5 @@ class Tictactoe extends Component {
   }
   
 }
-
 
 export default Tictactoe;
